2f18f8c58acd80b5b5ae79177b5a39cf8de921fa
tomasv <tomasvana@yahoo.com>
2023-06-05 11:32:26 +1000

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-s))
-
-    def whenFocused(self, entry): # Controls what happens when the user focuses on the entry (above bindings provide the functionality)
-        if entry.get() in self.entryTempText:
